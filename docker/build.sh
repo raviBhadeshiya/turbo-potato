5268b3b0fa97701f8684dcea4f6eb5699c3b8955
raviBhadeshiya <rnbhadeshiya@gmail.com>
2019-10-17 22:49:20 -0500

    modified flag

--- docker/build.sh
+++ docker/build.sh
@@ -8,6 +8,6 @@ docker build --build-arg BASE_IMAGE=$BASE_IMAGE \
     --build-arg ROS2_DISTRO=$ROS2_DISTRO \
     --build-arg DOCKER_USER=$DOCKER_USER \
     --build-arg DOCKER_USER=$DOCKER_USER \
-    --build-arg BUILD_AI=$BUILD_AI \
+    --build-arg BUILD_AI_PACKAGE=$BUILD_AI_PACKAGE \
     --build-arg TZ=$TZ \
     -t $CONTAINER_NAME .

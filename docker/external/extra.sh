b5f73da7ae0dd1038f7223d229dc3ff4fab5ac05
Ravi Bhadeshiya <5351081+raviBhadeshiya@users.noreply.github.com>
2022-02-02 20:51:07 -0600

    Delete docker directory

--- docker/external/extra.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-apt-get update && rm -rf /var/lib/apt/lists/*
--- docker/install/driver.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Setting up the drivers ---------------------------"
-sudo apt-get update 
-sudo apt-get upgrade -y
-sleep 1
-ubuntu-drivers devices
-sleep 1
-sudo ubuntu-drivers autoinstall
